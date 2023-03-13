3109ed10d7755ee036b05ee47312db18fd9d95e2
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-13 17:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-window.promises = [];
+const promises = [
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 5000)),
+];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+Promise.any(promises)
+  .then(result => {
+    const output = document.getElementById('output');
+    output.innerText = `The winner is ${result.toFixed(2)} seconds!`;
+  })
+  .catch(error => console.log(error));
